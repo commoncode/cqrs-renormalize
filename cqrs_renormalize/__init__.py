9e9ef9062195b38e0c71053d038693ca8eb42e13
Daryl Antony <daryl@commoncode.com.au>
2014-06-01 14:08:17 +1000

    added permission classes

--- cqrs_renormalize/__init__.py
+++ cqrs_renormalize/__init__.py
@@ -1,6 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.routers import SimpleRouter
 
+from oauth2_provider.ext.rest_framework import TokenHasReadWriteScope
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
 
 def create_router_for_backend(backend):
     """
@@ -14,6 +18,8 @@ def create_router_for_backend(backend):
     for name, collection in backend.collections.viewitems():
 
         class NewViewSet(ModelViewSet):
+            permission_classes = [IsAuthenticated, TokenHasReadWriteScope]
+
             queryset = collection.model.objects.all()
             serializer_class = collection.serializer_class
 
